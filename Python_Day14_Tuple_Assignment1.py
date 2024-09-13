5d92a21a0a5badd3817c839b94a430b3f0158b6c
Pragati2000 <34057938+Pragati2000@users.noreply.github.com>
2024-09-13 13:47:43 +0530

    Add files via upload

---
+++ Python_Day14_Tuple_Assignment1.py
@@ -0,0 +1,16 @@
+"""Write a Python program to find the number of times 4 appears in the tuple. 
+
+Input: tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7 ) Output: 3 """
+
+
+# Define the tuple
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Count the number of times 4 appears in the tuple
+count = tuplex.count(4)
+
+# Print the result
+print(count)
+
+
+#Output:3
